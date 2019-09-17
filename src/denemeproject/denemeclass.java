ce4db9e6cac518b445acbd7bb0f49beed1be0a0e
fikretgurkan <fikretgurkan2479@gmail.com>
2019-09-17 11:21:06 +0300

    tbtk ilk kayit

---
+++ src/denemeproject/denemeclass.java
@@ -0,0 +1,10 @@
+package denemeproject;
+
+public class denemeclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+	}
+
+}
